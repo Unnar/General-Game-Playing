afe4411c0f9e1572239be2f9f2c18fd76eb65812
Arnar Bjarni Arnarson <arnara14@ru.is>
2018-02-22 22:57:21 +0000

    UCT

--- ggp-base/src/main/java/org/ggp/base/player/gamer/statemachine/MonteCarloGamer.java
+++ ggp-base/src/main/java/org/ggp/base/player/gamer/statemachine/MonteCarloGamer.java
@@ -1,10 +1,12 @@
 package org.ggp.base.player.gamer.statemachine;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 import org.ggp.base.player.gamer.event.GamerSelectedMoveEvent;
 import org.ggp.base.player.gamer.statemachine.sample.SampleGamer;
+import org.ggp.base.util.gdl.grammar.GdlTerm;
 import org.ggp.base.util.statemachine.MachineState;
 import org.ggp.base.util.statemachine.Move;
 import org.ggp.base.util.statemachine.Role;
@@ -19,6 +21,9 @@ import is.ru.cadia.ggp.propnet.structure.GGPBasePropNetStructureFactory;
 public class MonteCarloGamer extends SampleGamer {
 	private Random random;
 	private MonteCarloNode root;
+
+	private final double C = 50;
+
 	public class TimeOutException extends Exception {
 		/**
 		 *
@@ -46,7 +51,26 @@ public class MonteCarloGamer extends SampleGamer {
 		long start = System.currentTimeMillis();
 		StateMachine machine = getStateMachine();
 		MachineState state = getCurrentState();
-		root = new MonteCarloNode(state);
+		List<GdlTerm> lastMoves = getMatch().getMostRecentMoves();
+		if (lastMoves != null) {
+			List<Move> jointMove = new ArrayList<Move>();
+			for (GdlTerm sentence : lastMoves)
+			{
+					jointMove.add(machine.getMoveFromTerm(sentence));
+			}
+			if(root.children.containsKey(jointMove)) {
+				root = root.children.get(jointMove);
+			}
+			else {
+				root = new MonteCarloNode(state);
+				expand(root);
+			}
+		} // else we are still in the initial state of the game
+		else
+		{
+			root = new MonteCarloNode(state);
+			expand(root);
+		}
 		Role r = getRole();
 
 //		HashMap<Move, Double> Q = new HashMap<Move, Double>();
@@ -58,9 +82,11 @@ public class MonteCarloGamer extends SampleGamer {
 		MonteCarloNode currNode = root;
 		try {
 			while(true) {
-				List<List<Move>> jointMoves = machine.getLegalJointMoves(currNode.state);
-				int choice = random.nextInt(jointMoves.size());
-				List<Move> jm = jointMoves.get(choice);
+				if(machine.isTerminal(currNode.state)) {
+					propagate(currNode, machine.getGoals(currNode.state));
+					currNode = root;
+				}
+				List<Move> jm = selectMoves(currNode);
 
 				if(currNode.children.containsKey(jm))
 				{
@@ -72,25 +98,10 @@ public class MonteCarloGamer extends SampleGamer {
 					MachineState nextState = machine.getNextState(currNode.state, jm);
 					MonteCarloNode newNode = new MonteCarloNode(nextState, currNode, jm);
 					currNode.children.put(jm, newNode);
-					int score = runSimulation(nextState, timeout-500);
+					expand(newNode);
+					List<Integer> scores = runSimulation(nextState, timeout-500);
 					newNode.simulations++;
-
-					while(currNode != null) {
-						currNode.simulations++;
-						for(Move m : jm) {
-							if(currNode.Q.containsKey(m)) {
-								currNode.Q.put(m, (currNode.Q.get(m)*currNode.N.get(m) + score)/(currNode.N.get(m)+1));
-								currNode.N.put(m, currNode.N.get(m)+1);
-							}
-							else {
-								currNode.Q.put(m, (double)score);
-								currNode.N.put(m, 1);
-							}
-						}
-
-						jm = currNode.parentMove;
-						currNode = currNode.parent;
-					}
+					propagate(newNode, scores);
 					currNode = root;
 				}
 //				Q.put(a, (Q.get(a)*N.get(a) + score)/(N.get(a)+1));
@@ -113,13 +124,15 @@ public class MonteCarloGamer extends SampleGamer {
 		return best;
 	}
 
-	private int runSimulation(MachineState state, long timeout) throws GoalDefinitionException, MoveDefinitionException, TransitionDefinitionException, TimeOutException {
+
+
+	private List<Integer> runSimulation(MachineState state, long timeout) throws GoalDefinitionException, MoveDefinitionException, TransitionDefinitionException, TimeOutException {
 		if(System.currentTimeMillis() > timeout) {
 			throw new TimeOutException("Minimax timed out");
 		}
 		StateMachine machine = getStateMachine();
 		if(machine.isTerminal(state)) {
-			return machine.getGoal(state, getRole());
+			return machine.getGoals(state);
 		}
 		List<List<Move>> jointMoves = machine.getLegalJointMoves(state);
 		int choice = random.nextInt(jointMoves.size());
@@ -128,4 +141,56 @@ public class MonteCarloGamer extends SampleGamer {
 		return runSimulation(nextState, timeout);
 	}
 
+	public List<Move> selectMoves(MonteCarloNode node) throws MoveDefinitionException {
+		StateMachine machine = getStateMachine();
+
+		ArrayList<Move> res = new ArrayList<Move>();
+		for(Role r : machine.getRoles()) {
+			Move bestMove = null;
+			double bestVal = -1;
+			for(Move m : machine.getLegalMoves(node.state, r)) {
+				double curr = UCT(node.Q.get(m), node.N.get(m), node.simulations);
+				if(bestVal < curr) {
+					bestMove = m;
+					bestVal = curr;
+				}
+			}
+			res.add(bestMove);
+		}
+
+		return res;
+	}
+
+	public double UCT(double q, double n, double sims) {
+		return (sims == 0 || n == 0) ? Double.POSITIVE_INFINITY : q + C * Math.sqrt(Math.log(sims)/n);
+	}
+
+	private void expand(MonteCarloNode node) throws MoveDefinitionException {
+		StateMachine machine = getStateMachine();
+		for(Role r : machine.getRoles()) {
+			for(Move m : machine.getLegalMoves(node.state, r)) {
+				node.Q.put(m, 0.0);
+				node.N.put(m, 0);
+			}
+		}
+	}
+
+	private void propagate(MonteCarloNode node, List<Integer> scores) {
+		while(node.parent != null) {
+			node.parent.simulations++;
+			for(int i = 0; i < node.parentMove.size(); i++) {
+				Move m = node.parentMove.get(i);
+				if(node.parent.Q.containsKey(m)) {
+					node.parent.Q.put(m, (node.parent.Q.get(m)*node.parent.N.get(m) + scores.get(i))/(node.parent.N.get(m)+1));
+					node.parent.N.put(m, node.parent.N.get(m)+1);
+				}
+				else {
+					node.parent.Q.put(m, (double)scores.get(i));
+					node.parent.N.put(m, 1);
+				}
+			}
+
+			node = node.parent;
+		}
+	}
 }
